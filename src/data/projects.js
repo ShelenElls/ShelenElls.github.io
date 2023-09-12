4489e91728cd06bfd6d1bf5ed117984a9312f70b
Shelen <shelen.ells@gmail.com>
2023-09-12 05:59:23 -0700

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,6 +11,19 @@ const data = [
       + ' Leveraged React Native for the front-end, employing various hooks and state elements for effective data manipulation and dynamic workout population.',
   },
   {
+    title: 'Pirates Booty',
+    subtitle: 'On the go workout APP',
+    image: '/images/projects/PiratesMainPage.jpg',
+    link: 'https://gitlab.com/hr-data-pirates/pirates-booty/-/blob/main-submission/README.md',
+    date: '2022-7-01',
+    desc:
+      'Assessed team’s design and plan for minimum-viable-product for initial release. '
+      + 'Collaborated with engineers to address and solve critical blocking issues throughout '
+      + 'application development while promoting an agile work environment. '
+      + 'Integrated API to dynamically populate workouts with a combination of two databases, data'
+      + ' data manipulation with different hook and state elements. '
+  },
+  {
     title: 'Conference GO',
     subtitle: 'Resource for in-person and virtual conference planner for attendees and presenters',
     image: '/images/projects/conferencego1.jpg',
@@ -34,18 +47,6 @@ const data = [
       + 'to ensure data integrity and horizontal scalability.'
       + 'Developed database communication by using polling techniques to share data between the microservices.'
       + 'Collaborated as a pair designing bounded contexts through the finished product.'
-  },
-  {
-    title: 'Sunflower',
-    subtitle: 'Passion Project',
-    image: '/images/projects/sunflower1.jpg',
-    link: 'https://github.com/ShelenElls/Sunflower-PersonalProject/blob/main/README.md',
-    date: '2022-5-01',
-    desc:
-      'Built for personal use at this stage. '
-      + 'Sunflower is a total income, debt, and goal management website '
-      + 'used to separate and organize different streams of debt and income '
-      + 'alongside predictive models to assist the average american in becoming debt free.',
   },
 ];
 
