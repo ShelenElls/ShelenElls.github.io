86a9324aeb8fcd2a03ca185bfdbd7ac46e2bcaa3
Shelen <shelen.ells@gmail.com>
2023-09-12 05:58:20 -0700

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,16 +1,14 @@
 const data = [
   {
-    title: 'Pirates Booty',
-    subtitle: 'On the go workout APP',
-    image: '/images/projects/PiratesMainPage.jpg',
-    link: 'https://gitlab.com/hr-data-pirates/pirates-booty/-/blob/main-submission/README.md',
-    date: '2022-7-01',
+    title: 'Transport Application',
+    subtitle: 'Community Schools',
+    image: '/images/projects/sunflower1.jpg',
+    link: 'n/a',
+    date: '2023-2-20',
     desc:
-      'Assessed team’s design and plan for minimum-viable-product for initial release. '
-      + 'Collaborated with engineers to address and solve critical blocking issues throughout '
-      + 'application development while promoting an agile work environment. '
-      + 'Integrated API to dynamically populate workouts with a combination of two databases, data'
-      + ' data manipulation with different hook and state elements. '
+      'Evaluated the team’s initial design and planning phase for a minimum-viable-product (MVP) release.' 
+      + 'Implemented an external API to retrieve both student and staff data, seamlessly integrating it with our Django backend. '
+      + ' Leveraged React Native for the front-end, employing various hooks and state elements for effective data manipulation and dynamic workout population.',
   },
   {
     title: 'Conference GO',
